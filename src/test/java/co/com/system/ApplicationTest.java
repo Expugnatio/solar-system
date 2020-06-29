73e24f4a4ecabd74d2e07bf62cbc67c5f83d768f
Christiam Hernandez <christiam.alejandro.hernandez@gmail.com>
2020-06-28 19:42:05 -0500

    :wrench: Add Integration/ Unit Tests Support

---
+++ src/test/java/co/com/system/ApplicationTest.java
@@ -0,0 +1,5 @@
+package co.com.system;
+
+public class ApplicationTest {
+
+}
--- src/test/java/co/com/system/service/CycleServiceTest.java
+++ src/test/java/co/com/system/service/CycleServiceTest.java
@@ -1,7 +1,10 @@
 package co.com.system.service;
 
+import static org.junit.Assert.assertTrue;
+
 import co.com.system.util.TestUtil;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,13 +16,15 @@ public class CycleServiceTest {
   @InjectMocks
   private CycleService cycleService;
 
-
-
   @Before
-  void setup(){
+  public void setup(){
     ReflectionTestUtils.setField(
         cycleService, "timeConfig", TestUtil.buildTimeConfig());
   }
 
+  @Test
+  public void whenCoordinatesFormTriangle_SunIsInTriangle_thenReturnSuccess(){
+    assertTrue(true);
+  }
 
 }
